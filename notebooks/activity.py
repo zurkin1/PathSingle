b2e0ea4dd9613b4eb34e3e11b4410aabc64cf7cf
Dani Livne <zurkin@yahoo.com>
2024-06-30 03:59:21 +0300

    change.

--- notebooks/activity.py
+++ notebooks/activity.py
@@ -3,6 +3,11 @@ import pandas as pd
 from tqdm import tqdm
 import gc
 import numpy as np
+#import cProfile
+#import pstats
+import multiprocessing as mp
+import scanpy as sc
+import scanpy.external as sce
 
 
 class path_activity:
@@ -14,9 +19,6 @@ class path_activity:
         #Load gene expression data.
         self.gene_expression_df = udp
         self.gene_expression_df.index = self.gene_expression_df.index.map(str.lower)
-
-        #Initialize cache.
-        self.activity_cache = {}
 
 
         #Split the 'source' and 'target' columns by '*' and lowercase the gene names.
@@ -44,22 +46,11 @@ class path_activity:
     #If a molecule does not have a probability we need to remove the whole interaction from activity and consistency calculations.
     #Reference https://www.kegg.jp/kegg/xml/docs/.
     #Function to calculate activity for a single interaction and a single sample.
-    def calculate_interaction_activity(self, row, sample):
-        sources = row['source']
-        interaction_type = row['interactiontype']
-        
-        #Calculate the product of gene values in the source column.
+    #Calculate the product of gene values in the source column.
+    def calculate_interaction_activity(self, sources, interaction_type, sample):
         activity = 1.0
         for gene in sources:
-            if gene in self.activity_cache:
-                gene_value = self.activity_cache[gene]
-            else:
-                try:
-                    gene_value = self.gene_expression_df.loc[gene][sample]
-                except KeyError:
-                    gene_value = 0.0  #Assume missing genes have a value of 0.
-                self.activity_cache[gene] = gene_value
-            
+            gene_value = self.gene_expression_df.at[gene, sample] if gene in self.gene_expression_df.index else 0.0
             activity *= gene_value
 
         #Adjust activity for different relation subtypes.
@@ -69,27 +60,40 @@ class path_activity:
         return activity
 
 
+    def process_sample(self, sample):
+        gc.collect()
+        sample_activities = []
+        for pathway in self.pathway_relations_df['pathway'].unique():
+            #Define dataframe for pathway.
+            pathway_data = self.pathway_relations_df[self.pathway_relations_df['pathway'] == pathway]
+            #activities = pathway_data.apply(lambda row: self.calculate_interaction_activity(row['source'], row['interactiontype'], sample), axis=1)
+            activities = [
+                            self.calculate_interaction_activity(row['source'], row['interactiontype'], sample)
+                            for _, row in pathway_data.iterrows()
+                        ]
+            pathway_activity = np.mean(activities, axis=0)
+            sample_activities.append([pathway, sample, pathway_activity])
+        return sample_activities
+
+
     def calculate_activity(self):
         #Calculate activity for each sample and each pathway.
         sample_names = self.gene_expression_df.columns
-        pathway_activities = pd.DataFrame(columns=['pathway', 'sample', 'activity'])
+        pathway_activities = []
+    
+        pool = mp.Pool()
+        results = [pool.apply_async(self.process_sample, args=(sample,)) for sample in sample_names]
+        
+        for result in tqdm(results, total=len(sample_names)):
+            pathway_activities.extend(result.get(timeout=100))
 
-        for sample in tqdm(sample_names):
-            self.activity_cache = {} #Clear the cache.
-            gc.collect()
-            for pathway in self.pathway_relations_df['pathway'].unique():
-                #Define dataframe for pathway.
-                pathway_data = self.pathway_relations_df[self.pathway_relations_df['pathway'] == pathway]
-                activities = pathway_data.apply(lambda row: self.calculate_interaction_activity(row, sample), axis=1)
-                pathway_activity = np.mean(activities, axis=0)
-                pathway_activities.loc[len(pathway_activities)+1] = [pathway, sample, pathway_activity]
-
-        #Create a DataFrame for the results and save to a CSV file.
-        self.output_df = pathway_activities.pivot(index='pathway', columns='sample', values='activity')
+        #Create a DataFrame for the results and save to a CSV file.        
+        pathway_activities_df = pd.DataFrame(pathway_activities, columns=['pathway', 'sample', 'activity'])
+        self.output_df = pathway_activities_df.pivot(index='pathway', columns='sample', values='activity')
         self.output_df.to_csv(self.output_file)
         print(f"Activity calculations saved to {self.output_file}")
 
-
+        
 def calc_activity_from_adata(adata):
     #On the index we have cell names (adata.obs_names) and the columns are gene names (adata.var_names). We transpose this dataframe before calculating activity.
     df = pd.DataFrame(data=adata.X, index=adata.obs_names, columns=adata.var_names).T
@@ -102,4 +106,14 @@ if __name__ == '__main__':
     udp.index = udp.index.map(str.lower)
     activity_obj = path_activity(udp)
     activity_obj.calculate_activity()
+    '''
+    profile_filename = './data/profile_output.prof'
+    cProfile.run('activity_obj.calculate_activity()', profile_filename)
+
+    with open('./data/profile_stats.txt', 'w') as f:
+        stats = pstats.Stats(profile_filename, stream=f)
+        stats.strip_dirs().sort_stats('cumulative').print_stats(10)
+    '''
 # %%
+adata = sc.read_h5ad('./data/sc_trainingmagic.h5ad')
+calc_activity_from_adata(adata)
