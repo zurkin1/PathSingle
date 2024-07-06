9b07ec76c1090397d75074b8dc048f87bba07e8f
Dani Livne <zurkin@yahoo.com>
2024-07-07 01:50:17 +0300

    change.

--- notebooks/activity.py
+++ notebooks/activity.py
@@ -149,106 +149,103 @@ import gc
 import scanpy as sc
 
 
-epsilon = 1e-10
-#Load gene expression data.
-#gene_expression_df = pd.read_csv('./data/sample_file.csv', index_col=0)
-#gene_expression_df.index = gene_expression_df.index.map(str.lower)
-adata = sc.read_h5ad('./data/sc_trainingmagic.h5ad')
-
-#Convert the gene expression data to a PyTorch tensor.
-gene_expression_tensor = torch.tensor(adata.X.T, dtype=torch.float32)  # Transpose to match the desired orientation
-
-#Create a mapping of gene names to their indices in the gene_expression_tensor.
-gene_names = adata.var_names.str.lower()
-gene_to_index = {gene: i for i, gene in enumerate(gene_names)}
-
-#Create a mapping of gene names to their indices in the gene_expression_tensor.
-#gene_to_index = {gene: i for i, gene in enumerate(gene_expression_df.index)}
-
-#Load pathway relations.
-pathway_relations = pd.read_csv('./data/pathway_relations.csv')
-
-#Ensure 'source' column is properly cleaned and converted to lists of strings.
-pathway_relations['source'] = pathway_relations['source'].fillna('').astype(str).str.lower().str.split('*')
-
-#Parse pathway relations to create a simplified data structure.
-pathway_interactions = {}
-
-
-#Function to determine if an interaction type is inhibitory.
-def is_inhibitory(interaction_type):
-    inhibitory_keywords = ['inhibition', 'repression', 'dissociation', 'missing interaction', 'dephosphorylation', 'ubiquitination']
-    return any(keyword in interaction_type for keyword in inhibitory_keywords)
-
-
-for _, row in pathway_relations.iterrows():
-    pathway = row['pathway']
-    sources = row['source']
-    inttype = row['interactiontype']
+def calc_activity(adata):
+    #Load gene expression data.
+    #gene_expression_df = pd.read_csv('./data/sample_file.csv', index_col=0)
+    #gene_expression_df.index = gene_expression_df.index.map(str.lower)
     
-    if pathway not in pathway_interactions:
-        pathway_interactions[pathway] = []
-    
-    pathway_interactions[pathway].append((sources,inttype))
+    #adata = sc.read_h5ad('./data/sc_trainingmagic.h5ad')
+    #adata = sc.pp.subsample(adata, fraction=0.2, copy=True)
+    print(adata)
 
-#Convert gene expression data to PyTorch tensor.
-#gene_expression_tensor = torch.tensor(gene_expression_df.values, dtype=torch.float32)
+    #Convert the gene expression data to a PyTorch tensor. Transpose to match the desired orientation.
+    gene_expression_tensor = torch.tensor(adata.X.T, dtype=torch.float32) #gene_expression_df.values
 
-#Define a Dataset.
-class GeneExpressionDataset(torch.utils.data.Dataset):
-    def __init__(self, gene_expression_tensor):
-        self.gene_expression_tensor = gene_expression_tensor
+    #Create a mapping of gene names to their indices in the gene_expression_tensor.
+    gene_names = adata.var_names.str.lower() #gene_expression_df.index
+    gene_to_index = {gene: i for i, gene in enumerate(gene_names)}
 
-    def __len__(self):
-        return self.gene_expression_tensor.shape[1]
+    #Load pathway relations.
+    pathway_relations = pd.read_csv('./data/pathway_relations.csv')
 
-    def __getitem__(self, idx):
-        return self.gene_expression_tensor[:, idx]
+    #Ensure 'source' column is properly cleaned and converted to lists of strings.
+    pathway_relations['source'] = pathway_relations['source'].fillna('').astype(str).str.lower().str.split('*')
 
-#Create DataLoader.
-batch_size = 5
-gene_expression_dataset = GeneExpressionDataset(gene_expression_tensor)
-gene_expression_loader = DataLoader(gene_expression_dataset, batch_size=batch_size, shuffle=False)
+    #Parse pathway relations to create a simplified data structure.
+    pathway_interactions = {}
 
-#Initialize a dictionary to store pathway activities.
-pathway_activities = {pathway: [] for pathway in pathway_interactions.keys()}
 
-#Process the multiplication in batches using DataLoader.
-for gene_expression_batch in tqdm(gene_expression_loader):
-    gene_expression_batch = gene_expression_batch.to('cpu')
-    
-    for sample in range(0,gene_expression_batch.shape[0]):
+    #Function to determine if an interaction type is inhibitory.
+    def is_inhibitory(interaction_type):
+        inhibitory_keywords = ['inhibition', 'repression', 'dissociation', 'missing interaction', 'dephosphorylation', 'ubiquitination']
+        return any(keyword in interaction_type for keyword in inhibitory_keywords)
 
-        #Calculate activities for each pathway.
-        for pathway, interactions in pathway_interactions.items():
-                pathway_activity = 0
-                interactions_counter = 0
-                for interaction in interactions:
-                    interaction_activity = 1
-                    for gene in interaction[0]:
-                        if gene in gene_to_index:
-                            interaction_activity *= gene_expression_batch[sample, gene_to_index[gene]]
+
+    for _, row in pathway_relations.iterrows():
+        pathway = row['pathway']
+        sources = row['source']
+        inttype = row['interactiontype']
+        
+        if pathway not in pathway_interactions:
+            pathway_interactions[pathway] = []
+        
+        pathway_interactions[pathway].append((sources,inttype))
+
+    #Define a Dataset.
+    class GeneExpressionDataset(torch.utils.data.Dataset):
+        def __init__(self, gene_expression_tensor):
+            self.gene_expression_tensor = gene_expression_tensor
+
+        def __len__(self):
+            return self.gene_expression_tensor.shape[1]
+
+        def __getitem__(self, idx):
+            return self.gene_expression_tensor[:, idx]
+
+    #Create DataLoader.
+    batch_size = 5
+    gene_expression_dataset = GeneExpressionDataset(gene_expression_tensor)
+    gene_expression_loader = DataLoader(gene_expression_dataset, batch_size=batch_size, shuffle=False)
+
+    #Initialize a dictionary to store pathway activities.
+    pathway_activities = {pathway: [] for pathway in pathway_interactions.keys()}
+
+    #Process the multiplication in batches using DataLoader.
+    for gene_expression_batch in tqdm(gene_expression_loader):
+        gene_expression_batch = gene_expression_batch.to('cpu')
+        
+        for sample in range(0,gene_expression_batch.shape[0]):
+
+            #Calculate activities for each pathway.
+            for pathway, interactions in pathway_interactions.items():
+                    pathway_activity = 0
+                    interactions_counter = 0
+                    for interaction in interactions:
+                        interaction_activity = 1
+                        for gene in interaction[0]:
+                            if gene in gene_to_index:
+                                interaction_activity *= gene_expression_batch[sample, gene_to_index[gene]]
+                        
+                        if is_inhibitory(interaction[1]):
+                            interaction_activity = 1 - interaction_activity
+                        pathway_activity += interaction_activity
+                        interactions_counter += 1
                     
-                    if is_inhibitory(interaction[1]):
-                        interaction_activity = 1 - interaction_activity
-                    pathway_activity += interaction_activity
-                    interactions_counter += 1
-                
-                #Calculate the mean activity for each pathway and for all cells.                
-                pathway_activity /= interactions_counter
-            
-                pathway_activities[pathway].append(pathway_activity) #.cpu().numpy())
+                    #Calculate the mean activity for each pathway and for all cells.                
+                    pathway_activity /= interactions_counter            
+                    pathway_activities[pathway].append(pathway_activity) #.cpu().numpy())
 
-mean_activity_matrix = np.zeros((len(adata.obs_names), len(pathway_interactions)))
+    mean_activity_matrix = np.zeros((len(adata.obs_names), len(pathway_interactions)))
 
-for idx, (pathway_name, activities) in enumerate(pathway_activities.items()):
-    if activities:
-        mean_activity_matrix[:, idx] = activities
-    else:
-        print(f"No activities for pathway {pathway_name}")
+    for idx, (pathway_name, activities) in enumerate(pathway_activities.items()):
+        if activities:
+            mean_activity_matrix[:, idx] = activities
+        else:
+            print(f"No activities for pathway {pathway_name}")
 
-#Create the DataFrame for the activity matrix.
-activity_df = pd.DataFrame(mean_activity_matrix, index=adata.obs_names, columns=list(pathway_interactions.keys())).T
+    #Create the DataFrame for the activity matrix.
+    activity_df = pd.DataFrame(mean_activity_matrix, index=adata.obs_names, columns=list(pathway_interactions.keys())).T
 
-#Save results to CSV.
-activity_df.to_csv('./data/output_activity.csv')
+    #Save results to CSV.
+    activity_df.T.to_csv('./data/output_activity.csv')
+# %%
