3ce97c295c19ba01e675fa9bf8b5927a71bf4c22
Dani Livne <zurkin@yahoo.com>
2024-12-08 02:48:07 +0200

    benchmark

--- pathsingle/benchmark.py
+++ pathsingle/benchmark.py
@@ -117,11 +117,9 @@ if __name__ == '__main__':
     sc.pp.filter_genes(adata, min_cells=1)  # Remove unexpressed genes. Keep genes expressed in at least 1 cell.
     #sc.pp.normalize_total(adata)  # Library size normalization (works on adata.X).
     #sc.pp.sqrt(adata)             # Square root transformation (works on adata.X).
-    adata.raw = adata.copy()      # Copy adata.X plus other objects to adata.raw.
+    #adata.raw = adata.copy()      # Copy adata.X plus other objects to adata.raw.
     #adata.X = scprep.normalize.library_size_normalize(adata.X) #For each cell (row), divide each expression value by the sum of the row and multiply by the scaling factor (default 1e4).
     #adata.X = scprep.transform.sqrt(adata.X) #For each value x in the expression matrix take √x. Stabilizes variance and reduces outliers.
-    # Handle NaNs if present.
-    adata.X = np.nan_to_num(adata.X, nan=0.0, posinf=0.0, neginf=0.0)
     
     # MAGIC imputation.
     #print(adata.X.toarray()[:5,:5]) #adata.raw.to_adata().X.toarray()
